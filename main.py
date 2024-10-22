bbb78339deeceaaa295952d661f2efe279b7c0d4
Szymon Wirkus <szymon.wirkus12@gmail.com>
2024-10-22 17:35:52 +0200

    (KNS) Szkielet GUI
    (KNS) Szkielet GUI cz. 2 (29.10.2024)
    
    Signed-off-by: Szymon Wirkus <szymon.wirkus12@gmail.com>
    
    (KNS) Szkielet GUI cz. 1 (22.10.2024)
    
    Signed-off-by: Szymon Wirkus <szymon.wirkus12@gmail.com>

--- main.py
+++ main.py
@@ -1,25 +1,48 @@
-import pygame
+import pygame as pg
+import random
 
-pygame.init()
-screen = pygame.display.set_mode((1280, 640))
-clock = pygame.time.Clock()
+pg.init()
+screen = pg.display.set_mode((400, 500))
+clock = pg.time.Clock()
 running = True
-pygame.display.set_caption("Digital World")
+pg.display.set_caption("Digital World")
 
+# Dodanie presetow kwadratow do planszy
+py: int = 0 # okresla y dla poszczególnych kwadratów
+plansza: list[object] = [] # tablica zawierajaca kwadraty
+for i in range(20):
+    px: int = 0 # okresla x dla poszczególnych kwadratów
+    plansza.append([])
+    for j in range(20):
+        plansza[i].append(pg.Rect(px, py, 20, 20))
+        px += 20
+    py += 20
+
+# Render
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    # Handling eventow
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             running = False
 
-    screen.fill("green")
+    screen.fill("black") # wypelnienie ekranu czarnym tlem
+    
+    # wyrysowanie planszy (nie trzeba tego robic)
+    for i in range(len(plansza)):
+        px = 0
+        for j in plansza[i]:
+            pg.draw.rect(screen, "white", j)
+            # print(f"Drawed rect in position: {px}, {py}")
+            px += 20
+        py += 20
 
-    # RENDER YOUR GAME HERE
+    xx = random.randint(0, 19)
+    xy = random.randint(0, 19)
+    pg.draw.rect(screen, "green", plansza[xx][xy])
 
     # flip() the display to put your work on screen
-    pygame.display.flip()
+    pg.display.flip()
 
     clock.tick(60)  # limits FPS to 60
 
-pygame.quit()
+pg.quit()
