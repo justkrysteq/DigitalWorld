87e5da9354fc60f6e947060c28f4598c8e935c83
Szymon Wirkus <szymon.wirkus12@gmail.com>
2024-11-18 07:25:28 +0100

    Dokumentacja main.py
    * Wprowadzenie komentarzy do kodu
    * Dodanie .DS_Store do .gitignore (kocham macos)
    
    Signed-off-by: Szymon Wirkus <szymon.wirkus12@gmail.com>
    Signed-off-by: ganomin <szymon.wirkus12@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+# Uruchom grę, jeśli wszystkie moduły są zainstalowane
 try:
-    import pygame as pg
-    import pygame_gui as gui
+    # Importowanie modułów
+    import pygame as pg # Sterowanie logiką gry
+    import pygame_gui as gui # Sterowanie GUI gry
     from random import randint
+    # Importowanie Świata i Organizmów
     from Swiat.Swiat import Swiat
     from Swiat.Organizmy.Zwierzeta.Wilk import Wilk
     from Swiat.Organizmy.Zwierzeta.Owca import Owca
@@ -12,10 +15,13 @@ try:
     from Swiat.Organizmy.Rosliny.Mlecz import Mlecz
     from Swiat.Organizmy.Rosliny.WilczeJagody import WilczeJagody
 
-    # Klasa Game - kontener gry
+    # Klasa Game
     class Game:
+        """
+        Główny kontener gry
+        """
         def __init__(self):
-            # Inicjalizacja okna
+            # Inicjalizacja okna Pygame
             pg.init()
             pg.display.set_caption("Wirtualny Świat - NA, KS, SW")
 
@@ -36,12 +42,15 @@ try:
             # Przypisanie klasy Swiat
             self.swiat = Swiat(self.N)
 
-            # Rysowanie guzików
+            # Tworzenie guzików
             self.next_round_button = gui.elements.UIButton(relative_rect=pg.Rect(10, 530, 150, 60), text="Wykonaj Turę", manager=self.manager, object_id=gui.core.ObjectID(class_id="@menu_control_button"))
             self.save_button = gui.elements.UIButton(relative_rect=pg.Rect(185, 530, 150, 60), text="Zapisz Świat", manager=self.manager, object_id=gui.core.ObjectID(class_id="@menu_control_button"))
             self.load_button = gui.elements.UIButton(relative_rect=pg.Rect(355, 530, 150, 60), text="Wczytaj Świat", manager=self.manager, object_id=gui.core.ObjectID(class_id="@menu_control_button"))
 
         def update_display(self):
+            """
+            Metoda odpowiedzialna za aktulizację świata z każdą turą 
+            """
             # Dictionary do przypisania odpowiednich nazw klasy stylów do klas organizmów
             class_to_name = {
                 Wilk: "wilk",
@@ -54,46 +63,67 @@ try:
                 WilczeJagody: "wilcze_jagody",
             }
 
-            # Wyświetlanie organizmów na podstawie tabeli organizmów z obiektu klasy Swiat
+            # Wyświetlanie organizmów na podstawie tabeli dwuwymiarowej organizmów z obiektu klasy Swiat
             organizmy = self.swiat.get_organizmy()
             for y in range(len(organizmy)):
                 for x in range(len(organizmy[y])):
                     # print("X:", organizmX, "Y:", organizmY)
-                    class_name = class_to_name.get(
-                        type(organizmy[y][x])
-                    )  # Przypisanie do zmiennej class_name nazwy klasy stylu z class_to_name jeśli organizm jest instancją zawartej tam klasy
+                    # Przypisanie do zmiennej class_name nazwy klasy stylu z class_to_name jeśli organizm jest instancją zawartej tam klasy
+                    class_name = class_to_name.get(type(organizmy[y][x]))
+                    
                     if class_name:
                         self.add_button(x, y, class_name)
                     else:
                         self.remove_button(x, y) # FIXME
 
-        # Wykonania nowej rundy
+        # Nowa runda
         def next_round(self):
+            """
+            Metoda wykonanie nowej rundy
+            """
             self.swiat.wykonajTure()
             self.update_display()
 
-        # Zapis świata
+        # Zapisanie świata
         def save_world(self):
+            """
+            Metoda wykonująca zapis świata
+            """
             pass
 
         # Wczytanie świata
         def load_world(self):
+            """
+            Metoda wykonująca wczytanie świata
+            """
             pass
 
-        # Wyświetlenie na planszy
+        # Wyświetlenie organizmu na planszy
         def add_button(self, x: int, y: int, classname: str, text: str = " "):
+            """
+            Metoda wykonująca wyświetlenie organizmu na polu [x, y]
+            """
             self.table[y][x] = gui.elements.UIButton(relative_rect=pg.Rect(x * 26, y * 26, 25, 25), text=text, manager=self.manager, object_id=gui.core.ObjectID(class_id=f"@{classname}"))
 
         # Usuwanie z planszy
         def remove_button(self, x: int, y: int):
+            """
+            Metoda wykonująca usunięcie organizmu z pola [x, y]
+            """
             self.table[y][x] = gui.elements.UIButton(relative_rect=pg.Rect(x * 26, y * 26, 25, 25), text=" ", manager=self.manager, object_id=gui.core.ObjectID(class_id="@puste_pole"))
 
+        # Stworzenie wszystkich organizmów
         def spawn_all(self) -> None:
+            """
+            Metoda wykonująca stworzenie wszystkich organizmów
+            """
             each_spawned_times = 2
             all_organisms = [Owca, Wilk, Lis, Mysz, Skunks, Trawa, Mlecz, WilczeJagody]
             used_positions = []
+            
             for _ in range(each_spawned_times):
                 for organizm in all_organisms:
+                    # Wybierz losowe wolne pole, obok już aktualnie zajętego
                     # position = [randint(0, self.N-1), randint(0, self.N-1)]
                     # while position in used_positions:
                     #     position = [randint(0, self.N-1), randint(0, self.N-1)]
@@ -102,8 +132,10 @@ try:
                     if organizm == Mysz or organizm == Lis:
                         for _ in range(10):
                             position = [randint(0, self.N-1), randint(0, self.N-1)]
+                            
                             while position in used_positions:
                                 position = [randint(0, self.N-1), randint(0, self.N-1)]
+                            
                             used_positions.append(position)
                             self.swiat.dodajOrganizm(organizm, position)
 
@@ -111,28 +143,39 @@ try:
         # Menu gry loop
         def menu(self) -> None:
             """
-            Metoda uruchamiająca menu
+            Metoda uruchamiająca menu gry
             """
             running = True
             game_state = "menu"
             clock = pg.time.Clock()
 
-   
+            # Tworzenie guzików do menu
             manager_menu = gui.UIManager((525, 630))
+            
+            # Guzik Graj
             play_button = gui.elements.UIButton(relative_rect=pg.Rect(160, 350, 180, 50),text="Graj",manager=manager_menu,object_id=gui.core.ObjectID(class_id="@menu_control_button"))
+            
+            # Guzik Ustawienia
             settings_button = gui.elements.UIButton(relative_rect=pg.Rect(160, 420, 180, 50),text="Ustawienia",manager=manager_menu,object_id=gui.core.ObjectID(class_id="@menu_control_button"))
+            
+            # Guzik Wyjście
             exit_button = gui.elements.UIButton(relative_rect=pg.Rect(160, 490, 180, 50),text="Wyjście",manager=manager_menu,object_id=gui.core.ObjectID(class_id="@menu_control_button"))   
+           
+            # Guzik Powrót
             back_to_menu_button = gui.elements.UIButton(relative_rect=pg.Rect(160, 550, 180, 50),text="Powrót",manager=manager_menu,object_id=gui.core.ObjectID(class_id="@menu_control_button"),visible=False)
+            
+            # Pole wpisania wielkości planszy
             number_input = gui.elements.UITextEntryLine(relative_rect=pg.Rect(300, 130, 150, 50),manager=manager_menu,object_id=gui.core.ObjectID(class_id="@menu_control_button"),visible=False,initial_text="20") 
 
-
+            # Menu loop
             while running:
                 time_delta = clock.tick(60) / 1000.0
                 for event in pg.event.get():
                     if event.type == pg.QUIT:
                         running = False
                         return "quit"
-
+                    
+                    # Logika guzików w menu
                     if event.type == gui.UI_BUTTON_PRESSED:
                         if game_state == "menu":
                             if event.ui_element == play_button:
@@ -147,7 +190,8 @@ try:
                                 back_to_menu_button.show()
                                 number_input.show()
                             elif event.ui_element == exit_button:
-                                return "quit" 
+                                return "quit"
+                            
                         elif game_state == "settings":
                             if event.ui_element == back_to_menu_button:
                                 game_state = "menu"
@@ -156,29 +200,32 @@ try:
                                 exit_button.show()
                                 back_to_menu_button.hide()
                                 number_input.hide()
+
+                    # Logika pola wpisania tekstu
                     if event.type == gui.UI_TEXT_ENTRY_FINISHED and event.ui_element == number_input:
                         user_n_input = number_input.get_text()
                         print(user_n_input, "test")
+
                         if user_n_input.isdigit():
                             N_value = int(user_n_input)
                             if N_value > 5 and N_value < 30:
                                 self.N = N_value
                                 self.table = [[None for _ in range(self.N)] for _ in range(self.N)]
                                 self.swiat = Swiat(self.N)
-                            
                                 print(f"Wprowadzona liczba: {user_n_input}")
                             else:
                                 print("Niepoprawna liczba")
                         else:
-                            number_input.set_text("")           
+                            number_input.set_text("")
                             print("Niepoprawna liczba")
-                           
+
                     manager_menu.process_events(event)
 
-        # Wyświetlanie menu
+                # Wyświetlenie menu
                 self.screen.fill((100, 100, 100))
-                if game_state == "menu":
 
+                if game_state == "menu":
+                    # Konfiguracja interfejsu menu
                     font = pg.font.Font(None,54)
                     font_version = pg.font.Font(None,36)
                     font_credits = pg.font.Font(None, 16)
@@ -194,14 +241,16 @@ try:
                     self.screen.blit(credits_1, (380, 560))
                     self.screen.blit(credits_2, (380, 580))
                     self.screen.blit(credits_3, (380, 600))
+
                 elif game_state == "settings":
+                    # Konfiguracja interfejsu ustawień
                     font = pg.font.Font(None,54)
                     settings_title = font.render("Opcje",True, (255,255,255))
                     self.screen.blit(settings_title,(200,50))
                     font_option_n = pg.font.Font(None, 24)
                     setting_n = font_option_n.render("Podaj wielkość planszy NxN (6-29):", True, (255, 255, 255))
                     self.screen.blit(setting_n,(20,150))
-        
+
                 manager_menu.update(time_delta)
                 manager_menu.draw_ui(self.screen)
                 pg.display.flip()
@@ -217,6 +266,7 @@ try:
             clock = pg.time.Clock()
             running = True
 
+            # Stwórz wszystkie organizmy
             self.spawn_all()
             # Miejsce na testy
             # self.swiat.dodajOrganizm(Owca, 0, 0)
@@ -230,7 +280,7 @@ try:
             # self.swiat.dodajOrganizm(Trawa, 2, 0)
             # self.swiat.dodajOrganizm(Wilk, 4, 5)
 
-            # Wyświetlenie początkowego stanu tabeli
+            # Wyświetlenie początkowego tury świata
             self.update_display()
 
             # Pętla gry
@@ -265,5 +315,6 @@ try:
             app.run()
         pg.quit()
 
+# Przechwytywanie wyjątku, jeśli wymagany moduł nie jest znaleziony
 except ModuleNotFoundError as module:
     print(f"Upewnij się, że masz wszystkie zależności oraz pliki, dlatego że: {module}")
